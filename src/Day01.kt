b12d2328fd416bc97b129550ac7e389de590fe69
afranken <coding@techotronic.de>
2024-12-05 00:16:23 +0100

    Day 1 Part 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,18 +1,33 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val inputPairs = input.map { s ->
+            val (a, b) = s.split("   ")
+            a to b
+        }
+
+        val leftSorted = inputPairs.map { s -> s.first.toInt() }.sorted()
+        val rightSorted = inputPairs.map { s -> s.second.toInt() }.sorted()
+
+        val sortedPairs = leftSorted.zip(rightSorted)
+
+        val distances = sortedPairs.map { pair ->
+            if (pair.first > pair.second) {
+                pair.first - pair.second
+            } else {
+                pair.second - pair.first
+            }
+        }
+
+        return distances.sum()
     }
 
     fun part2(input: List<String>): Int {
         return input.size
     }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
     // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 11)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
