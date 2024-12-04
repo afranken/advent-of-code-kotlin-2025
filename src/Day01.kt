cbd3495dfe299212d97e15325ba0fbfde287e639
afranken <coding@techotronic.de>
2024-12-05 00:24:22 +0100

    Day 1 Part 2

--- src/Day01.kt
+++ src/Day01.kt
@@ -22,7 +22,25 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val inputPairs = input.map { s ->
+            val (a, b) = s.split("   ")
+            a to b
+        }
+
+        val leftSorted = inputPairs.map { s -> s.first.toInt() }.sorted()
+        val rightSorted = inputPairs.map { s -> s.second.toInt() }.sorted()
+
+        val rightMultiplier = Array(999999) { 0 }
+
+        rightSorted.forEach{ value ->
+            rightMultiplier[value]++
+        }
+
+        val similarities = leftSorted.map { value ->
+            value * rightMultiplier[value]
+        }
+
+        return similarities.sum()
     }
 
     // Or read a large test input from the `src/Day01_test.txt` file:
@@ -32,5 +50,10 @@ fun main() {
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
     part1(input).println()
+
+    // Or read a large test input from the `src/Day01_test.txt` file:
+    val testInput2 = readInput("Day01_test")
+    check(part2(testInput2) == 31)
+
     part2(input).println()
 }
